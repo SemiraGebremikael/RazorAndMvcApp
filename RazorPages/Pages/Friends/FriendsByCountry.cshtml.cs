3b3b7462e5c9809681fd6c4ff0c123a1ba6330de
semiragebremikael <semiragere61@gmail.com>
2023-12-22 12:03:31 +0100

    add csMainDecontext

---
+++ RazorPages/Pages/Friends/FriendsByCountry.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using DbModels;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Models;
+using RazorPages.Pages.Friends;
+using Services;
+
+namespace RazorPages.Pages.Friends
+{
+    public class FriendsByCountryModel : PageModel
+    {
+        IFriendsService _friendsService = null;
+
+        ILogger<FriendsByCountryModel> _logger = null;
+
+        public List<csFriend> Friend { get; set; } = new List<csFriend>();
+
+        public void OnGet()
+        {
+            var uri = Request.Path;
+
+            //Use the Service
+            Friends = _friendsService.ReadFriendsAsync();
+            return Page();
+        }
+    }
+
+    public class FriendsByCountryModel(IFriendsService service, ILogger<FriendsByCountryModel> logger)
+    {
+        _logger = logger;
+        _friendsService = service;
+    }
+}
+
